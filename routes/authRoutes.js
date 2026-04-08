b3a58c9b51f39b67c53ea6aa8081317df7cd09c4
ThanushkaMalshan <thanushkamalshan88@gmail.com>
2026-04-08 19:16:46 +0530

    creating the forget password mechanism

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { registerUser, loginUser, googleAuth, getUserProfile, updateUserProfile } from '../controllers/authentication.js';
+import { registerUser, loginUser, googleAuth, getUserProfile, updateUserProfile, forgotPassword, verifyOTP, resetPassword } from '../controllers/authentication.js';
 import { protect } from '../middleware/auth.js';
 
 const router = express.Router();
@@ -7,6 +7,9 @@ const router = express.Router();
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/google', googleAuth);
+router.post('/forgot-password', forgotPassword);
+router.post('/verify-otp', verifyOTP);
+router.post('/reset-password', resetPassword);
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
 
 export default router;
